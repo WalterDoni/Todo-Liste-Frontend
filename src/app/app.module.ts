6efbbfacd875d4e1a2b0c3164a9b0ae6ac959e66
Walter Doni <walter.doni@gmx.at>
2024-02-08 15:31:02 +0100

    start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { AlltodosComponent } from './components/alltodos/alltodos.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AlltodosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/alltodos/alltodos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alltodos',
+  templateUrl: './alltodos.component.html',
+  styleUrls: ['./alltodos.component.scss']
+})
+export class AlltodosComponent {
+
+}
