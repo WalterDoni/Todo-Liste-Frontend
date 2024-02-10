e1f35909a8b054d6e04195d1718896ec971e5285
WalterDoni <133245257+WalterDoni@users.noreply.github.com>
2024-02-10 14:33:14 +0100

    progress

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -3,15 +3,34 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
-  email: string="";
-  password: string="";
+  username: string = '';
+  password: string = '';
 
+  async loginWithUsernameAndPassword() {
+    const myHeaders = new Headers();
+    myHeaders.append('Content-Type', 'application/json');
 
+    const raw = JSON.stringify({
+      username: this.username,
+      password: this.password,
+    });
 
-  login(){
-    
+    const requestOptions: RequestInit = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow',
+    };
+    try {
+      let resp = await fetch('http://127.0.0.1:8000/login/', requestOptions);
+      let json = await resp.json();
+      localStorage.setItem('token', json.token)  
+    } catch(e){
+     console.log(e);
+     
+    }
   }
 }
